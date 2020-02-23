f1b58618dea1ab257b89a988365d653a860a895d
Alireza Mirzaee <teacher.mirzaee@gmail.com>
2020-02-23 18:03:40 +0330

    Add AuthType Feature

--- app/src/main/java/com/github/alishir/apn/MainActivity.kt
+++ app/src/main/java/com/github/alishir/apn/MainActivity.kt
@@ -23,14 +23,25 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-        val apn = apn_txt.text
-        val mcc = mcc_txt.text
-        val mnc = mnc_txt.text
-        val type = type_txt.text
-        val editable = if (editable_chk.isChecked) "1" else "0"
-        val cmd = "content insert --uri content://telephony/carriers --bind user_editable:i:${editable} --bind name:s:\"${apn}\" --bind numeric:s:\"${mcc}${mnc}\" --bind type:s:\"${type}\" --bind mcc:i:${mcc} --bind mnc:s:${mnc} --bind apn:s:${apn} --bind roaming_protocol:s:IP --bind protocol:s:IP"
-        val delete_cmd = "content delete --uri content://telephony/carriers --where \"mcc='${mcc}' and mnc='${mnc}'\""
         volte_btn.setOnClickListener {
+            val apn = apn_txt.text
+            val mcc = mcc_txt.text
+            val mnc = mnc_txt.text
+            val type = type_txt.text
+            val editable = if (editable_chk.isChecked) "1" else "0"
+
+            var authtype = -1
+            if ( auth_type_spinner.selectedItemId == 1L )
+                authtype = 0;
+            if ( auth_type_spinner.selectedItemId == 2L )
+                authtype = 1;
+            else if ( auth_type_spinner.selectedItemId == 3L )
+                authtype = 2;
+            else if ( auth_type_spinner.selectedItemId == 4L )
+                authtype = 3;
+
+            val cmd = "content insert --uri content://telephony/carriers --bind user_editable:i:${editable} --bind edited:i:${editable} --bind authtype:i:${authtype} --bind name:s:\"${apn}\" --bind numeric:s:\"${mcc}${mnc}\" --bind type:s:\"${type}\" --bind mcc:i:${mcc} --bind mnc:s:${mnc} --bind apn:s:${apn} --bind roaming_protocol:s:IP --bind protocol:s:IP"
+
             try {
                 val su = Runtime.getRuntime().exec("su")
                 val dos = DataOutputStream(su.getOutputStream())
@@ -49,6 +60,9 @@ class MainActivity : AppCompatActivity() {
             try {
                 val su = Runtime.getRuntime().exec("su")
                 val dos = DataOutputStream(su.getOutputStream())
+                val mcc = mcc_txt.text
+                val mnc = mnc_txt.text
+                val delete_cmd = "content delete --uri content://telephony/carriers --where \"mcc='${mcc}' and mnc='${mnc}'\""
                 dos.writeBytes("${delete_cmd}\n")
                 dos.flush()
                 dos.close()
