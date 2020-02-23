a87fa7bae8d5acd1ba4c1d0bb4118fc814d72344
Ali <aj.shirvani@gmail.com>
2020-02-23 15:23:40 +0330

    remove exit.

--- app/src/main/java/com/github/alishir/apn/MainActivity.kt
+++ app/src/main/java/com/github/alishir/apn/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.github.alishir.apn
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -8,6 +9,10 @@ import java.io.DataOutputStream
 
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG = "SINA-APN"
+    }
 
     private lateinit var su: Process
 
@@ -24,13 +29,17 @@ class MainActivity : AppCompatActivity() {
         val mnc = mnc_txt.text
         val cmd = "content insert --uri content://telephony/carriers --bind name:s:\"${apn}\" --bind numeric:s:\"${mcc}${mnc}\" --bind type:s:\"ims\" --bind mcc:i:${mcc} --bind mnc:s:${mnc} --bind apn:s:${apn} --bind roaming_protocol:s:IP --bind protocol:s:IP"
         volte_btn.setOnClickListener {
-            val dos = DataOutputStream(su.getOutputStream())
-            dos.writeBytes("${cmd}\n")
-            dos.writeBytes("exit\n")
-            dos.flush()
-            dos.close()
-            su.waitFor()
-            Toast.makeText(applicationContext, "${cmd}", Toast.LENGTH_LONG).show()
+            try {
+                val dos = DataOutputStream(su.getOutputStream())
+                dos.writeBytes("${cmd}\n")
+                dos.flush()
+                dos.close()
+                su.waitFor()
+                Toast.makeText(applicationContext, "${cmd}", Toast.LENGTH_LONG).show()
+            } catch (e: Exception) {
+                Toast.makeText(applicationContext, "Error: ${e.message}", Toast.LENGTH_LONG).show()
+                Log.e(TAG, "${e.message}")
+            }
         }
     }
 }
